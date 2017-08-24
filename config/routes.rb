160127a22dfdfd74be05703c218e48a87cff13fc
anthonyshikanga <anthonyshikanga@gmail.com>
2017-08-24 11:41:08 +0300

    modified routes.rb with singular recource for cart

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :orders
   resources :order_items
   resources :products
+  resource :cart, only: [:show]
+  
 
   root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
